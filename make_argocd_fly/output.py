4a8c42a7001cc65e8a0ba2e7879afb2638e63375
Andrei <karandash8@gmail.com>
2023-07-03 11:52:25 +0200

    - added support for multi resources in a single file

--- make_argocd_fly/output.py
+++ make_argocd_fly/output.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 
 log = logging.getLogger(__name__)
 
@@ -9,9 +10,18 @@ class OutputRenderer:
     self.output_dir = output_dir
     self.envs = envs
 
-  def write_file(self, relative_dir: str, filename: str, content: str) -> None:
+  def write_file(self, relative_dir: str, filename: str, resource: str) -> None:
     path = os.path.join(self.output_dir, relative_dir)
     os.makedirs(path, exist_ok=True)
 
-    with open(os.path.join(path, filename), 'w') as f:
-      f.write(content)
+    if not os.path.exists(os.path.join(path, filename)):
+      with open(os.path.join(path, filename), 'w') as f:
+        f.write(resource)
+    else:
+      with open(os.path.join(path, filename), 'r') as f:
+        file_content = f.read()
+
+      if not re.search(resource, file_content):
+        with open(os.path.join(path, filename), 'a') as f:
+          f.write('---')
+          f.write(resource)
