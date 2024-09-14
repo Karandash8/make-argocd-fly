d1e47259708bbc07d6f3e40917feb127acc1034d
Andrei <karandash8@gmail.com>
2024-09-14 11:25:47 +0200

    feat: add jinja2 extention to include multiple files

--- make_argocd_fly/renderer.py
+++ make_argocd_fly/renderer.py
@@ -1,9 +1,12 @@
 import logging
 import os
+import re
 import socket
+import typing as t
 from abc import ABC, abstractmethod
 from jinja2 import Environment, BaseLoader, FunctionLoader, nodes, StrictUndefined
 from jinja2.ext import Extension
+from jinja2.exceptions import TemplateNotFound
 from markupsafe import Markup
 
 from make_argocd_fly.resource import ResourceViewer
@@ -38,6 +41,46 @@ class DigExtension(Extension):
       return None
 
 
+class CustomFunctionLoader(FunctionLoader):
+  def __init__(
+        self,
+        load_func: t.Callable[
+            [str],
+            t.Optional[
+                t.Union[
+                    str, t.Tuple[str, t.Optional[str], t.Optional[t.Callable[[], bool]]]
+                ]
+            ],
+        ],
+        render_func: t.Callable[
+            [str],
+            t.Optional[
+                t.Union[
+                    str, t.Tuple[str, t.Optional[str], t.Optional[t.Callable[[], bool]]]
+                ]
+            ],
+        ],
+        list_func: t.Callable[[str], t.List[str]] = None,
+  ) -> None:
+    super().__init__(load_func)
+    self.render_func = render_func
+    self.list_func = list_func
+
+  def get_rendered(self, environment: "Environment", template: str) -> t.Tuple[str, t.Optional[str], t.Optional[t.Callable[[], bool]]]:
+    rv = self.render_func(template)
+
+    if rv is None:
+      raise TemplateNotFound(template)
+
+    if isinstance(rv, str):
+      return rv, None, None
+
+    return rv
+
+  def list_templates(self, path: str) -> t.List[str]:
+    return self.list_func(path)
+
+
 class IncludeRawExtension(Extension):
   tags = {"include_raw"}
 
@@ -48,17 +91,48 @@ class IncludeRawExtension(Extension):
     return nodes.Output([result], lineno=lineno)
 
   def _render(self, filename):
-    return Markup(self.environment.loader.get_source(self.environment, filename)[0])
+    loaded_template = self.environment.loader.get_source(self.environment, filename)
+    content = loaded_template[0] if loaded_template else ''
+
+    return Markup(content)
+
+
+class IncludeAllAsYamlKVExtension(Extension):
+  tags = {"include_all_as_yaml_kv"}
+
+  def parse(self, parser):
+    lineno = parser.stream.expect("name:include_all_as_yaml_kv").lineno
+    template = parser.parse_expression()
+    result = self.call_method("_render", [template], lineno=lineno)
+    return nodes.Output([result], lineno=lineno)
+
+  def _render(self, path: str) -> str:
+    children = self.environment.loader.list_templates(path)
+    kv_as_yaml_str = []
+
+    for child in children:
+      if child.name.endswith('.j2'):
+        loaded_template = self.environment.loader.get_rendered(self.environment, child.element_rel_path)
+        child_name = child.name[:-3]
+      else:
+        loaded_template = self.environment.loader.get_source(self.environment, child.element_rel_path)
+        child_name = child.name
+
+      child_content = loaded_template[0] if loaded_template else ''
+      kv_as_yaml_str.append('{}: |\n  {}\n'.format(child_name, re.sub('\n', '\n  ', child_content.strip())))
+
+    return Markup(''.join(kv_as_yaml_str))
 
 
 class JinjaRenderer(AbstractRenderer):
   def __init__(self, viewer: ResourceViewer = None) -> None:
     self.viewer = viewer
     if viewer:
-      self.loader = FunctionLoader(self._get_template)
+      self.loader = CustomFunctionLoader(self._get_source, self._get_rendered, self._list_templates)
     else:
       self.loader = BaseLoader()
     self.env = Environment(extensions=[IncludeRawExtension,
+                                       IncludeAllAsYamlKVExtension,
                                        DigExtension,
                                        'jinja2_ansible_filters.AnsibleCoreFiltersExtension'],
                            loader=self.loader, undefined=StrictUndefined)
@@ -66,7 +140,7 @@ class JinjaRenderer(AbstractRenderer):
     self.template_vars = {}
     self.filename = '<template>'
 
-  def _get_template(self, path: str):
+  def _get_source(self, path: str):
     files_children = self.viewer.get_files_children(os.path.basename(path))
     for file_child in files_children:
       if file_child.element_rel_path == path:
@@ -74,6 +148,27 @@ class JinjaRenderer(AbstractRenderer):
 
     log.error('Missing template {}'.format(path))
     return None
+
+  def _get_rendered(self, path: str):
+    if not path.endswith('.j2'):
+      log.error('Template {} is not a jinja template'.format(path))
+      return None
+
+    files_children = self.viewer.get_files_children(os.path.basename(path))
+    for file_child in files_children:
+      if file_child.element_rel_path == path:
+        return (self.render(file_child.content), path, None)
+
+    log.error('Missing template {}'.format(path))
+    return None
+
+  def _list_templates(self, path: str) -> t.List[str]:
+    element = self.viewer.get_element(path)
+    if not element or not element.is_dir:
+      log.error('Provided path {} is not a directory'.format(path))
+      return []
+
+    return element.get_files_children('.+')
 
   def set_template_vars(self, template_vars: dict) -> None:
     self.template_vars = template_vars
